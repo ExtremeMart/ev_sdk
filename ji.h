248dc8bf6f2a0ec091616efe31dc80255d1dd85b
LaurenLuoYun <lauren.luo@foxmail.com>
2018-03-14 17:42:59 +0800

    update ji.h, add input args

--- ji.h
+++ ji.h
@@ -4,7 +4,7 @@
 /*
  * Notes:
  * 1. If use init funciton, no need to release library or model.
- * 2. Please release image inside the functions(ji_calc).
+ * 2. Please release image inside the functions(ji_calc_*).
  *
  */
 
@@ -25,8 +25,8 @@ extern "C" {
    /*
       define call back function 
       parameters:
-        code : event code (1:warning event, 2:statistics event)
-        json : event
+        code : event code
+        json : event (JSON format)
       return :
         0:success
         others : fail
@@ -46,37 +46,53 @@ extern "C" {
       parameters:
         buffer : input file buffer
         length : input file buffer length
+        args  : if any, input parameters (JSON format)
         outfn : output file name
-        json : result json string （JSON format）
+        json : result json string (JSON format)
       return :
         0:success
         others : fail
     */ 
-    int ji_calc(const unsigned char* buffer, int length, const char* outfn, char** json);
+    int ji_calc(const unsigned char* buffer, int length, const char* args, const char* outfn, char** json);
 
+   /*
+      analysis image
+      parameters:
+        infn : input file name
+        outfn : output file name
+        args  : if any, input parameters (JSON format)
+        json : result json string (JSON format)
+      return :
+        0:success
+        others : fail
+    */ 
+    int ji_calc_file(const char* infn, const char* args, const char* outfn, char** json);
+    
    /*
       analysis video file
       parameters:
         infn : input file name
+        args  : if any, input parameters(JSON format)
         outfn : output file name
         cb : call back function point 
       return :
         0:success
         others : fail
     */ 
-    int ji_calc_video_file(const char* infn, const char* outfn, JI_CB cb);
+    int ji_calc_video_file(const char* infn, const char* args, const char* outfn, JI_CB cb);
 
    /*
       analysis video frame
       parameters:
         inframe : input frame
+        args  : if any,input parameters (JSON format)
         outframe : output frame
         cb : call back function point 
       return :
         0:success
         others : fail
     */     
-    int ji_calc_video_frame(JI_CV_FRAME* inframe, JI_CV_FRAME** outframe, JI_CB cb);
+    int ji_calc_video_frame(JI_CV_FRAME* inframe, const char* args, JI_CV_FRAME** outframe, JI_CB cb);
     
 }
 #endif
