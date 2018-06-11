53a3a46dd1a25c85b65f42aaf33896111a1a3f99
extreme assistant <developer@extremevision.com.cn>
2018-06-11 14:19:14 +0800

    New version of ji.h

--- ji.h
+++ ji.h
@@ -2,9 +2,9 @@
 #define JI_H
 
 /*
- *  如果是图片分析，需要实现ji_init,ji_create_predictor,ji_calc_file,ji_calc_buffer,ji_destory_predictor;
- *
- *  如果是视频分析，需要实现ji_init,ji_create_predictor,ji_calc_video_file,ji_calc_video_frame.
+ * Notes:
+ * 1. If use init funciton, no need to release library or model.
+ * 2. Please release image inside the functions(ji_calc).
  *
  */
 
@@ -30,6 +30,13 @@ extern "C" {
 		int code; //event code
 		char* json; //event
 	}JI_EVENT;
+	
+	typedef struct
+	{
+		const char* filename;
+		const unsigned char* buffer;
+		int length;
+	}JI_FILE_BUFFER;
 
     /* 
         init library/model
@@ -44,7 +51,10 @@ extern "C" {
 	*/
     void *ji_create_predictor();
 
-
+	/*
+	destory instance
+	*/
+    void ji_destory_predictor(void* predictor);
    /*
       analysis image
       parameters:
@@ -56,7 +66,21 @@ extern "C" {
         0:success
         others : fail
     */ 
-    int ji_calc_buffer(void* predictor, const unsigned char* buffer, int length, const char* args, const char* outfn, char** json);
+    int ji_calc(void* predictor, const unsigned char* buffer, int length, const char* args, const char* outfn, char** json);
+    
+	/*
+      analysis image
+      parameters:
+        predictor : predictor created by ji_create_predictor()
+        inBuffer : input file buffer
+        args : input args
+        outBuffer : output file buffer
+        json : result json string 
+      return :
+        0:success
+        others : fail
+    */ 
+    int ji_calc_buffer(void* predictor, JI_FILE_BUFFER *inBuffer, const char* args, JI_FILE_BUFFER *outBuffer, char** json);
 
    /*
       analysis image
@@ -75,7 +99,7 @@ extern "C" {
       parameters:
         infn : input file name
         outfn : output file name
-        event : result event
+        cb : call back function point 
       return :
         0:success
         others : fail
@@ -87,17 +111,60 @@ extern "C" {
       parameters:
         inframe : input frame
         outframe : output frame
-        event : result event
+        cb : call back function point 
       return :
         0:success
         others : fail
     */     
     int ji_calc_video_frame(void* predictor, JI_CV_FRAME* inframe, const char* args, JI_CV_FRAME* outframe, JI_EVENT* event);
     
-    /*
-     destory instance
-     */
-    void ji_destory_predictor(void* predictor);
-    
 }
+
+/* 
+如果是图片分析，请实现以下方法
+*/
+
+    /* 
+        init library/model
+        return:
+          0:success
+          other : fail 
+    */
+    int ji_init(int argc, char** argv);
+
+    /*
+    creat instance and initiallized that can be used to analysis
+	*/
+    void *ji_create_predictor();
+
+	/*
+	destory instance
+	*/
+    void ji_destory_predictor(void* predictor);
+
+   /*
+      analysis image
+      parameters:
+        buffer : input file buffer
+        length : input file buffer length
+        outfn : output file name
+        json : result json string 
+      return :
+        0:success
+        others : fail
+    */ 
+    int ji_calc(void* predictor, const unsigned char* buffer, int length, const char* args, const char* outfn, char** json);
+
+   /*
+      analysis image
+      parameters:
+        infn : input file name
+        outfn : output file name
+        json : result json string 
+      return :
+        0:success
+        others : fail
+    */ 
+    int ji_calc_file(void* predictor, const char* infn, const char* args, const char* outfn, char** json);
+
 #endif
