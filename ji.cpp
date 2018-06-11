b448c2c774c5d4a73779b48ab08f81429da6c752
extreme assistant <developer@extremevision.com.cn>
2018-06-11 11:37:22 +0800

    update ji.h

--- ji.cpp
+++ ji.cpp
@@ -12,23 +12,51 @@
 
 using namespace std;
 
-int ji_init()
+int ji_init(int argc, char** argv)
 {
     return 0;
 }
 
-int ji_calc(const unsigned char* buffer, int length, const char* outfn, char** json)
+void *ji_create_predictor()
 {
-    return 0;
+	return NULL;
 }
 
-int ji_calc_video_file(const char* infn, const char* outfn, JI_CB cb)
+void ji_destory_predictor(void* predictor)
 {
-//	cb(1, "json");
+	
+}
+    
+int ji_calc_file(void* predictor, const char* infn, const char* args, const char* outfn, char** json)
+{
 	return 0;
 }
 
-int ji_calc_video_frame(JI_CV_FRAME* inframe, JI_CV_FRAME** outframe, JI_CB cb)
+int ji_calc(void* predictor, const unsigned char* buffer, int length, const char* args, const char* outfn, char** json)
+{
+	//buffer -> vector
+	vector<unsigned char> decodebuf(buffer,buffer+length);
+	
+	//vector --> cv::Mat
+//	cv::Mat img =cv::imdecode(cv::Mat(decodebuf), 1);
+
+    //read file ---> cv::Mat
+//  cv::Mat img =cv::imread(filename, 1);
+
+    return 0;
+}
+
+int ji_calc_buffer(void* predictor, JI_FILE_BUFFER *inBuffer, const char* args, JI_FILE_BUFFER *outBuffer, char** json)
+{
+	return 0;
+}
+
+int ji_calc_video_file(void* predictor, const char* infn, const char* args, const char* outfn, JI_EVENT* event)
+{
+	return 0;
+}
+
+int ji_calc_video_frame(void* predictor, JI_CV_FRAME* inframe, const char* args, JI_CV_FRAME* outframe, JI_EVENT* event)
 {
 	return 0;
 }
