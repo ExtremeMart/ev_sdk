66884dcfc41d877a024093e58f393974dd482a06
extreme assistant <developer@extremevision.com.cn>
2018-09-27 15:10:30 +0800

    Fix the error
    Fix the error

--- ji_util.hpp
+++ ji_util.hpp
@@ -1,43 +1,46 @@
 #ifndef JI_UTIL_HPP
 #define JI_UTIL_HPP
 
-#include <fstream>
+#include <fstream>  
 #include <iostream>
 #include <iterator>
 #include <vector>
 #include <sstream>
 
 using namespace std;
+
 extern "C"{
 
-        /*
-         * convert image file to buffer.
-         * parameters:
-         *      in_filename : input file name
-         *      out_buffer  : output file buffer
-         * return:
-         *      0:success
-         *      other:fail
-         */
-        int ji_file2buffer(const char* in_filname, vector<unsigned char>* out_buffer);
-        
-        int ji_file2buffer(const char* in_filename, vector<unsigned char>* out_buffer){
-                ifstream in(in_filename, ios::in|ios::binary);
-                if(!in){ 
-                        cout<<"file cannot read."<<endl;
-                        return 1;
-                }
-        //      stringstream sstr;
-        //      while(in>>sstr.rdbuf());
-                istreambuf_iterator<char> start(in), end;
-                vector<unsigned char> buffer(start, end);
-                in.close();
-                
-   //           vector<unsigned char> buffer(sstr.str().begin(), sstr.str().end());
-                *out_buffer = buffer;
-                return 0;
-        }       
-        
+	/*
+	 * convert image file to buffer.
+	 * parameters:
+	 * 	in_filename : input file name
+	 * 	out_buffer  : output file buffer
+	 * return:
+	 * 	0:success
+	 * 	other:fail
+	 */
+
+	int ji_file2buffer(std::string inFileName, vector<unsigned char>* outBuffer){
+		ifstream fin(inFileName.c_str(), ios::binary);
+		if(!fin){
+			cout<<"file cannot read."<<endl;
+			return -1;
+		}
+
+		stringstream sstr;
+		sstr << fin.rdbuf();
+
+		fin.close();
+		vector<unsigned char> buffer;
+		for(int i=0; i < sstr.str().size(); i++){
+			buffer.push_back(sstr.str()[i]);
+		}
+
+		*outBuffer = buffer;
+		return 0;
+	}
+
 }
 
 #endif // JI_UTIL_H
