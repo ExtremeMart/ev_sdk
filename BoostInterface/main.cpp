26ad9721b179912e42f28836b62a10b7da6a1d80
extreme assistant <developer@extremevision.com.cn>
2018-12-05 09:28:03 +0800

    Update main.cpp

--- BoostInterface/main.cpp
+++ BoostInterface/main.cpp
@@ -8,6 +8,7 @@
 
 #include <iostream>
 #include "BoostInterface.h"
+//请注意，roi感兴趣区域 分为3种，点，线，面，其中面为多边形（例如，三角形，或矩形，或者其他） 下列有对应的写法
 //char * roiPoint = "POINT(0.9 0.5)|POINT(0.9 0.5)";这是多点写法（多线段和多个多边形类似）里面的参数 为比例分割 例如： 0.1 = 1/10
 // 极市小工具网址  http://dev.polygonlinepointtool.extremevision.com.cn/   
 
@@ -19,21 +20,21 @@ int main(int argc, char* argv[]){
 	vector<cv::Point> pointPolygon;
 	//解析线段
 	std::cout<<"test"<<std::endl;
-	char * roiLine = "LINESTRING(0.1 0.4, 0.9 0.5)";
+	char * roiLine = "LINESTRING(0.1 0.4, 0.9 0.5)"; //------------------线------------------
 	boostInterface.parseLinestring(roiLine, curSize, pointLine);
 	for(int i = 0; i < pointLine.size(); i++){
 		cout<<"line"<<pointLine[i].x<<endl;
 		cout<<"line"<<pointLine[i].y<<endl;
 	}
 	//解析点
-	char * roiPoint = "POINT(0.9 0.5)";
+	char * roiPoint = "POINT(0.9 0.5)";//------------------点------------------
 	boostInterface.parsePoint(roiPoint, curSize, point);
 	for(int i = 0; i < point.size(); i++){
 		cout<<"point"<<point[i].x<<endl;
 		cout<<"point"<<point[i].y<<endl;
 	}
 	//解析多边形
-	char * roiPolygon = "POLYGON((0.1 0.4, 0.9 0.5,0.35 0.4,0.66 0.66, 0.78 0.78))";
+	char * roiPolygon = "POLYGON((0.1 0.4, 0.9 0.5,0.35 0.4,0.66 0.66, 0.78 0.78))";//------------------面------------------
 	boostInterface.parsePolygon(roiPolygon, curSize, pointPolygon);
 	for(int i = 0; i < pointPolygon.size(); i++){
 		cout<<"polygon"<<pointPolygon[i].x<<endl;
