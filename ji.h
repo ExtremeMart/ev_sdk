28695e5d90f83ec93ed29ec20b5e844059498c1f
extreme assistant <developer@extremevision.com.cn>
2018-12-05 09:23:51 +0800

    Update ji.h

--- ji.h
+++ ji.h
@@ -68,8 +68,9 @@ void ji_destory_predictor(void* predictor);
  @para1：检测器实例
  @para2: 输入图片文件Buffer,统一用C++标准库进行图片到二进制流转换,自行参考ji_file2buffer(imgfile,buffer)函数,请勿用opencv等其它图像处理库函数对图片进行转换（自行管理图片缓存释放）
  @para3: 输入图片Buffer长度
- @para4: 可选项,图片感兴趣区域等绘制（规范请看 极市文档 BoostInterface ）
- @para5: 输出文件名称（自行管理图片缓存释放）（如果需要保存，需要在算法内部实现，传入的参数是一个绝对路径,如JSON内有image_path选项，则优先判断是否有指定输出保存，如没有则默认image_path 路径保存，例如工目录下../dest/xxx.jpg）de
+ @para4: 可选项,图片感兴趣区域等绘制（传入的ROI感兴趣区域为不固定 规范请看 极市文档 BoostInterface ）
+ @para5: 输出文件名称（自行管理图片缓存释放）（如果需要保存，需要在算法内部实现，传入的参数是一个绝对路径,
+ 如JSON内有image_path选项，则优先判断是否有指定输出保存，如没有则默认image_path 路径保存，例如工目录下../dest/xxx.jpg）
  @para6: 分析图片输出Json信息,主函数中释放JSON
  return :
  0:success
@@ -85,8 +86,9 @@ int ji_calc(void* predictor, const unsigned char* buffer, int length,
  parameters:
  @para1：检测器实例
  @para2: 输入图片文件名称（传入的参数是一个绝对路径）
- @para3: 可选项,视频感兴趣区域的绘制（规范请看 极市文档 BoostInterface ）
- @para4: 输出文件名称（如果需要保存，需要在算法内部实现，传入的参数是一个绝对路径）
+ @para3: 可选项,视频感兴趣区域的绘制（传入的ROI感兴趣区域为不固定 规范请看 极市文档 BoostInterface ）
+ @para4: 输出文件名称（如果需要保存，需要在算法内部实现，传入的参数是一个绝对路径,
+ 如JSON内有image_path选项，则优先判断是否有指定输出保存，如没有则默认image_path 路径保存，例如工目录下../dest/xxx.jpg）
  @para5: 分析图片输出Json信息,主函数中释放JSON
  @调用此接口
  @return :
@@ -101,7 +103,7 @@ int ji_calc_file(void* predictor, const char* infn, const char* args,
  analysis video file（视频处理必选项）
  @para1: 检测器实例
  @para2: 输入视频地址（传入的参数是一个绝对路径）
- @para3: 可选项,图片感兴趣区域等的绘制（规范请看 极市文档 BoostInterface ）
+ @para3: 可选项,图片感兴趣区域等的绘制（传入的ROI感兴趣区域为不固定 规范请看 极市文档 BoostInterface ）
  @para4: 输出视频地址（如果需要保存，需要在算法内部实现，传入的参数是一个绝对路径）
  @para5: 分析视频Json信息,主函数中释放Json
  @return：
@@ -116,7 +118,7 @@ int ji_calc_video_file(void* predictor, const char* infn, const char* args,
  analysis video frame（视频处理必选项）
  @para1: 检测器实例
  @para2: 输入单帧 （自行管理帧释放）
- @para3: 可选项,单帧分析感兴趣区域的绘制等（规范请看 极市文档 BoostInterface ）
+ @para3: 可选项,单帧分析感兴趣区域的绘制等（传入的ROI感兴趣区域为不固定 规范请看 极市文档 BoostInterface ）
  @para4: 输出单帧 （自行管理帧释放）（返回处理后的单帧，是否保存由调用者决定）
  @para5: 分析视频Json信息,主函数中释放Json
  @return：
