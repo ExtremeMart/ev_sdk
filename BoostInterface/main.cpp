b85d3dee6c884f70abc6addf1002666e1ca8a581
extreme assistant <developer@extremevision.com.cn>
2019-01-09 10:51:53 +0800

    Update main.cpp

--- BoostInterface/main.cpp
+++ BoostInterface/main.cpp
@@ -11,7 +11,7 @@
 //请注意，roi感兴趣区域 分为3种，点，线，面，其中面为多边形（例如，三角形，或矩形，或者其他） 下列有对应的写法
 //char * roiPoint = "POINT(0.9 0.5)|POINT(0.9 0.5)";这是多点写法（多线段和多个多边形类似）里面的参数 为比例分割 例如： 0.1 = 1/10
 // 极市小工具网址  http://dev.polygonlinepointtool.extremevision.com.cn/   
-// 请注意（重点！！！！！！）：感兴趣区域是什么就要画出什么。例如 是五边形在输出的结果上就需要画出5边形
+// 请注意（重点！！！！！！）：感兴趣区域是什么就要画出什么。例如 是五边形在输出的结果上就需要画出五边形
 int main(int argc, char* argv[]){
 	BoostInterface boostInterface;
 	cv::Size curSize(1920, 1080);
